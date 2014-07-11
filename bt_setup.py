73bec8427befeb0a4b73251f3f2ebbfcd95202d4
Alex Jakimenko <alex.jakimenko@gmail.com>
2014-07-11 21:57:44 +0300

    Small script to configure bluetooth modules

---
+++ bt_setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+import serial
+from serial import SerialException
+
+sio = serial.Serial(self.device, 9600)
+
+#TODO would this even work?
+
+def chat(sio, str):
+    sio.write(str + "\r\n")
+    print sio.readline()
+
+chat(sio, "AT+RENEW")
+chat(sio, "AT+VER?")
+
+chat(sio, "AT+NAMEProgArm")
+chat(sio, "AT+PIN1234")
+
+chat(sio, "AT+BAUD2") # 0: 9600 1: 19200 2: 38400 3: 57600 4: 115200
+chat(sio, "AT+STOP0") # 0: one stop bit 1: two stop bits
+chat(sio, "AT+PARI1") # 0: none 1: even 2: odd
